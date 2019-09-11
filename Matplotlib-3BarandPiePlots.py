1fd17d9191673038d3389f303c22b453e612f78e
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-09-11 10:51:25 +0200

    Create Matplotlib-3BarandPiePlots.py

---
+++ Matplotlib-3BarandPiePlots.py
@@ -0,0 +1,35 @@
+####
+Writing Functions
+Complete the following function definitions, defined in Matsession/tests/test_plots.py, based on the instructions provided:
+
+1. test_barplot_of_iris_sepal_length
+
+Create a figure of size 8 inches in width, and 6 inches in height. Name it as fig.
+
+Create an axis, associated with figure fig, using add_subplot. Name it as ax.
+
+Define a list species, with elements ['setosa', 'versicolor', 'viriginica'].
+
+Define a list index, with values [0.2, 1.2, 2.2].
+
+Define another list sepal_len with values [5.01, 5.94, 6.59]. These values represent the mean sepal length of iris flowers belonging to three species.
+
+Draw a bar plot using the bar function, such that the height of each vertical bar displays the sepal length of a species.
+
+Use index and sepal_len as variables. Set bar width as 0.5, color as red, and border color of the bar as black.
+
+Label X-Axis as Species.
+
+Label Y-Axis as Sepal Length (cm).
+
+Set Title as Mean Sepal Length of Iris Species.
+
+Limit X-Axis from 0 to 3.
+
+Limit Y-Axis from 0 to 7.
+
+Set ticks on X-Axis at 0.45, 1.45, and 2.45.
+
+Set X-Axis tick labels to ['setosa', 'versicolor', 'viriginica']
+####
+
