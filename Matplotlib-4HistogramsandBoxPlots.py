90ae531557db99ff511b5bc5915082da84bbabfb
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-09-11 11:42:30 +0200

    Create Matplotlib-4HistogramsandBoxPlots.py

---
+++ Matplotlib-4HistogramsandBoxPlots.py
@@ -0,0 +1,23 @@
+####
+Writing Functions
+Complete the following function definitions, defined in Matsession/tests/test_plots.py, based on the instructions provided:
+
+1. test_hist_of_a_sample_normal_distribution
+
+Create a figure of size 8 inches in width, and 6 inches in height. Name it as fig.
+
+Create an axis, associated with figure fig, using add_subplot. Name it as ax.
+
+Set random seed to 100 using the expression np.random.seed(100).
+
+Create a normal distribution x1 of 1000 values, with mean 25 and standard deviation 3.0. Use np.random.randn.
+
+Draw a histogram of x1 with 30 bins. Use the hist function.
+
+Label X-Axis as X1
+
+Label Y-Axis as Bin Count
+
+Set Title as Histogram of a Single Dataset.
+####
+
